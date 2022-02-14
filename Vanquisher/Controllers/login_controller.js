8c8756db66b9e339d2599dee4f325125f33fd986
kashishbhandula <kashishbhandula112@gmail.com>
2022-02-14 16:40:30 +0530

    Sign up part completed

--- Vanquisher/Controllers/login_controller.js
+++ Vanquisher/Controllers/login_controller.js
@@ -1,14 +1,50 @@
+const { redirect } = require('express/lib/response');
+const User=require('../Models/credential_schema');
 module.exports.sign_in=function(req,res){
 
-
-
-
-   
-    res.render('sign_in.ejs');
+        res.render('sign_in.ejs');
     return;
 };
 module.exports.sign_up=function(req,res){
     
     res.render('sign_up.ejs');
     return;
+};
+module.exports.create_profile=function(req,res){
+
+   if(req.body.password!=req.body.confirm_password)
+   {
+       console.log("Password and Confirm Password are different");
+       return;
+   }
+   else
+   {
+       User.findOne({email:req.body.email},function(err,user){
+           if(!user)
+           {
+            User.create(req.body,function(err,user){
+                if(err)
+                {
+                    console.log("Error in creating User");
+                    
+                    return res.redirect('back');
+                }
+                else
+                {
+                    res.render('sign_in');
+                    return;
+                }
+                
+            })
+           }
+           else
+           {
+            res.render('sign_in');
+            return;
+               
+           }
+       })
+   }
+
+
 };
