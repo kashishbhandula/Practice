b0336397daf68085783b9bbb0473c2c99e0e04c0
kashishbhandula <kashishbhandula112@gmail.com>
2022-02-14 16:54:05 +0530

    Sign In Completed

--- Vanquisher/Routers/login.js
+++ Vanquisher/Routers/login.js
@@ -11,6 +11,7 @@ const Login_controller=require('../Controllers/login_controller');
 Router.get('/sign_up',Login_controller.sign_up);
 Router.get('/sign_in',Login_controller.sign_in);
 Router.post('/create_profile',Login_controller.create_profile);
+Router.post('/profile',Login_controller.create_session);
 
 
 
